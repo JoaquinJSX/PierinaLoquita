cec248cd1df502437e6851887c3ff7b6a07b3a75
Joaquin <JoaquinJSX>
2025-05-24 17:20:48 -0500

    Uploading filed

---
+++ script.js
@@ -0,0 +1,14 @@
+const mainMessage = document.querySelector('#main_text');
+
+mainMessage.innerHTML = 'Pierina, me gustas tanto que a veces con solo mirarte me siento embriagado.';
+
+function changeMainText() {
+    setTimeout(() => {
+        mainMessage.innerHTML = '¿Sabes?';
+    }, 4500);
+    setTimeout(() => {
+        mainMessage.innerHTML = 'Mientras más te veo, más quedo encantado con tu cuerpo; tu sonrisa, tus hipnotizantes ojos y cada cosa que logro apreciar de ti...';
+    }, 7000);
+}
+
+changeMainText();
